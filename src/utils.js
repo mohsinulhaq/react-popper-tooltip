78a60140c8e958b2b728de68c1db6cf075a46ac4
Denis Borovikov <metaller.ru@gmail.com>
2018-08-16 15:11:55 +0800

    Use render props for trigger and tooltip

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,2 @@
-import React from 'react';
-import cn from 'classnames';
-import _ from 'lodash';
-
-export const renderSlot = (El, extraProps = {}) => {
-  if (_.isFunction(El)) {
-    let props = extraProps;
-    if (_.isFunction(extraProps)) {
-      props = props();
-    }
-    return <El {...props} />;
-  }
-  if (React.isValidElement(El)) {
-    let props = extraProps;
-    if (_.isFunction(extraProps)) {
-      props = props(El.props);
-    } else if (props.className) {
-      props.className = cn(props.className, El.props.className);
-    }
-    return React.cloneElement(El, props);
-  }
-  return El;
-};
+export const callAll = (...fns) => (...args) =>
+  fns.forEach(fn => fn && fn(...args));
