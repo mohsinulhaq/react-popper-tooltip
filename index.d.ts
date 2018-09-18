ec1216fe63e058e29584ca4f7469b2e38902f7c9
Johan Brook <johan@lookback.io>
2018-09-18 18:58:26 +0200

    Fix `arrowPlacement` and `arrowStyle` to latest API

--- index.d.ts
+++ index.d.ts
@@ -7,72 +7,75 @@ declare module 'react-popper-tooltip' {
   import Popper from 'popper.js';
 
   type GetTriggerProps = (
-      props?: {
-          onClick?: (evt: React.SyntheticEvent) => void;
-          onContextMenu?: (evt: React.SyntheticEvent) => void;
-          onMouseEnter?: (evt: React.SyntheticEvent) => void;
-          onMouseLeave?: (evt: React.SyntheticEvent) => void;
-          [key: string]: any;
-      }
-  ) => {
-      onClick: (evt: React.SyntheticEvent) => void;
-      onContextMenu: (evt: React.SyntheticEvent) => void;
-      onMouseEnter: (evt: React.SyntheticEvent) => void;
-      onMouseLeave: (evt: React.SyntheticEvent) => void;
+    props?: {
+      onClick?: (evt: React.SyntheticEvent) => void;
+      onContextMenu?: (evt: React.SyntheticEvent) => void;
+      onMouseEnter?: (evt: React.SyntheticEvent) => void;
+      onMouseLeave?: (evt: React.SyntheticEvent) => void;
       [key: string]: any;
+    }
+  ) => {
+    onClick: (evt: React.SyntheticEvent) => void;
+    onContextMenu: (evt: React.SyntheticEvent) => void;
+    onMouseEnter: (evt: React.SyntheticEvent) => void;
+    onMouseLeave: (evt: React.SyntheticEvent) => void;
+    [key: string]: any;
   };
 
   export default class TooltipTrigger extends React.PureComponent<Props, {}> {
-      showTooltip(): void;
+    showTooltip(): void;
 
-      hideTooltip(): void;
+    hideTooltip(): void;
 
-      toggleTooltip(): void;
+    toggleTooltip(): void;
 
-      scheduleShow(): void;
+    scheduleShow(): void;
 
-      scheduleHide(): void;
+    scheduleHide(): void;
 
-      scheduleToggle(): void;
+    scheduleToggle(): void;
 
-      getTriggerProps: GetTriggerProps;
+    getTriggerProps: GetTriggerProps;
   }
 
   interface ChildProps {
-      getTriggerProps: GetTriggerProps;
-      triggerRef: React.RefObject<any>;
+    getTriggerProps: GetTriggerProps;
+    triggerRef: React.RefObject<any>;
   }
 
   interface TooltipProps {
-      getTooltipProps: (
-          props?: {
-              style?: React.CSSProperties;
-              onMouseLeave?: (evt: React.SyntheticEvent) => void;
-              onMouseEnter?: (evt: React.SyntheticEvent) => void;
-              [key: string]: any;
-          }
-      ) => {
-          style: React.CSSProperties;
-          onMouseEnter: (evt: React.SyntheticEvent) => void;
-          onMouseLeave: (evt: React.SyntheticEvent) => void;
-          [key: string]: any;
-      };
-      tooltipRef: React.RefObject<any>;
-      arrowStyle: () => React.CSSProperties;
-      arrowRef: React.RefObject<any>;
-      arrowPlacement: Popper.Placement;
+    getTooltipProps: (
+      props?: {
+        style?: React.CSSProperties;
+        onMouseLeave?: (evt: React.SyntheticEvent) => void;
+        onMouseEnter?: (evt: React.SyntheticEvent) => void;
+        [key: string]: any;
+      }
+    ) => {
+      style: React.CSSProperties;
+      onMouseEnter: (evt: React.SyntheticEvent) => void;
+      onMouseLeave: (evt: React.SyntheticEvent) => void;
+      [key: string]: any;
+    };
+    tooltipRef: React.RefObject<any>;
+    getArrowProps: (props: any) => {
+      style: React.CSSProperties;
+      [key: string]: any;
+    };
+    arrowRef: React.RefObject<any>;
+    placement: Popper.Placement;
   }
 
   export interface Props {
-      tooltip: (props: TooltipProps) => JSX.Element;
-      children: (props: ChildProps) => JSX.Element;
-      defaultTooltipShown?: boolean;
-      tooltipShown?: boolean;
-      delayShow?: number;
-      delayHide?: number;
-      trigger?: 'click' | 'hover' | 'right-click' | 'none';
-      closeOnOutOfBoundaries?: boolean;
-      modifiers?: Popper.Modifiers;
-      placement?: Popper.Placement;
+    tooltip: (props: TooltipProps) => JSX.Element;
+    children: (props: ChildProps) => JSX.Element;
+    defaultTooltipShown?: boolean;
+    tooltipShown?: boolean;
+    delayShow?: number;
+    delayHide?: number;
+    trigger?: 'click' | 'hover' | 'right-click' | 'none';
+    closeOnOutOfBoundaries?: boolean;
+    modifiers?: Popper.Modifiers;
+    placement?: Popper.Placement;
   }
 }
