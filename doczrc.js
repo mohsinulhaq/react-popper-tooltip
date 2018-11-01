25a588ec8aa2a3fe2454a0b915b54526d29a14e5
mohsinulhaq <mohsinulhaq01@gmail.com>
2018-11-02 03:10:44 +0530

    remove codesandbox option from docz

--- doczrc.js
+++ doczrc.js
@@ -4,5 +4,6 @@ export default {
   title: 'react-popper-tooltip',
   description: 'Documentation',
   base: '/react-popper-tooltip/',
-  plugins: [css()]
+  plugins: [css()],
+  codeSandbox: false
 };
