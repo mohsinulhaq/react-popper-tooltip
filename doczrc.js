5dc648d077cac57655f7114ddeeea27a97c20f90
mohsinulhaq <mohsinulhaq01@gmail.com>
2018-12-09 15:07:24 +0530

    remove codeSandbox integration option from docs

--- doczrc.js
+++ doczrc.js
@@ -1,8 +1,9 @@
-import { css } from 'docz-plugin-css';
+import {css} from 'docz-plugin-css';
 
 export default {
   title: 'react-popper-tooltip',
   description: 'Documentation',
   base: '/react-popper-tooltip/',
+  codeSandbox: false,
   plugins: [css()]
 };
