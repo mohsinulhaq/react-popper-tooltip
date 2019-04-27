2b2105a01f50f11165b87bc6ded7db6b755d549a
mohsinulhaq <mohsinulhaq01@gmail.com>
2019-04-27 17:33:29 +0530

    upgrade docz, move docs to netlify

--- doczrc.js
+++ doczrc.js
@@ -2,8 +2,7 @@ import {css} from 'docz-plugin-css';
 
 export default {
   title: 'react-popper-tooltip',
-  description: 'Documentation',
-  base: '/react-popper-tooltip/',
+  description: 'Documentation for react-popper-tooltip',
   typescript: true,
   codeSandbox: false,
   plugins: [css()]
