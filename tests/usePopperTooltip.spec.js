499631cfc224e388ce031482b0eee4a88ae37bb8
Denis Borovikov <metaller.ru@gmail.com>
2020-12-07 16:43:59 -0700

    Add more tests

--- tests/usePopperTooltip.spec.js
+++ tests/usePopperTooltip.spec.js
@@ -38,16 +38,6 @@ function Tooltip({ options }) {
   );
 }
 
-//
-// beforeEach(() => {
-//   jest.useFakeTimers()
-// })
-//
-// afterEach(() => {
-//   jest.runOnlyPendingTimers()
-//   jest.useRealTimers()
-// })
-
 describe('trigger option', () => {
   test('hover works', async () => {
     render(<Tooltip options={{ trigger: 'hover' }} />);
@@ -128,24 +118,81 @@ describe('trigger option', () => {
     // Nothing after hover
     userEvent.hover(screen.getByText(TriggerText));
     jest.runAllTimers();
-    expect(await screen.queryByText(TooltipText)).not.toBeInTheDocument();
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
 
     // Nothing after click
     userEvent.click(screen.getByText(TriggerText));
     jest.runAllTimers();
-    expect(await screen.queryByText(TooltipText)).not.toBeInTheDocument();
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
 
     // Nothing after right-click
     fireEvent.contextMenu(screen.getByText(TriggerText));
     jest.runAllTimers();
-    expect(await screen.queryByText(TooltipText)).not.toBeInTheDocument();
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
 
     // Nothing after focus
     fireEvent.focus(screen.getByText(TriggerText));
     jest.runAllTimers();
-    expect(await screen.queryByText(TooltipText)).not.toBeInTheDocument();
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
 
     jest.runOnlyPendingTimers();
     jest.useRealTimers();
   });
+
+  test('array of triggers works', async () => {
+    render(<Tooltip options={{ trigger: ['click', 'hover'] }} />);
+    // Initially there's no tooltip
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+    // Show on hover
+    userEvent.hover(screen.getByText(TriggerText));
+    expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+    // Hide on click
+    userEvent.click(screen.getByText(TriggerText));
+    await waitFor(() => {
+      expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+    });
+  });
+});
+
+test('closeOnClickOutside option works', async () => {
+  render(<Tooltip options={{ closeOnClickOutside: true, trigger: 'click' }} />);
+
+  // Show on click
+  userEvent.click(screen.getByText(TriggerText));
+  expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+  // Hide on body click
+  userEvent.click(document.body);
+  await waitFor(() => {
+    expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+  });
+});
+
+test('initialVisible option works', async () => {
+  render(<Tooltip options={{ initialVisible: false }} />);
+  expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+  render(<Tooltip options={{ initialVisible: true }} />);
+  expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+});
+
+test('delayShow option works', async () => {
+  jest.useFakeTimers();
+  render(<Tooltip options={{ delayShow: 5000 }} />);
+
+  userEvent.hover(screen.getByText(TriggerText));
+  // Nothing after a 2000ms
+  jest.advanceTimersByTime(2000);
+  expect(screen.queryByText(TooltipText)).not.toBeInTheDocument();
+
+  // It shows up somewhen after. Here RTL uses fake timers to await as well, so
+  // it awaits for the element infinitely, advancing jest fake timer by 50ms
+  // in an endless loop. And this is why the test passes even if delayShow set
+  // a way bigger than default timeout of 1000ms would allow.
+  expect(await screen.findByText(TooltipText)).toBeInTheDocument();
+
+  jest.runOnlyPendingTimers();
+  jest.useRealTimers();
 });
