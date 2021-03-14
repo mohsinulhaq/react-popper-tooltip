022384167cebec8220e9cf5377b6545509204083
Vaclav Grohling <grohling.v@clav.cz>
2021-03-14 14:19:18 +0100

    fix: Wrong positioning

--- src/usePopperTooltip.ts
+++ src/usePopperTooltip.ts
@@ -69,7 +69,7 @@ export function usePopperTooltip(
   });
 
   const timer = React.useRef<number>();
-  React.useEffect(() => () => clearTimeout(timer.current));
+  React.useEffect(() => () => clearTimeout(timer.current), []);
 
   const { styles, attributes, ...popperProps } = usePopper(
     finalConfig.followCursor ? virtualElement : triggerRef,
